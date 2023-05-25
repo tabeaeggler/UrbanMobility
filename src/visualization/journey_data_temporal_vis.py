2292e8ebe6fc64cad3fc069305887754083be670
Tabea <tabea.eggler@students.fhnw.ch>
2023-05-25 14:39:06 +0100

    created correlation table for temporal features -> not relevant as ordinal data

---
+++ src/visualization/journey_data_temporal_vis.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
+import matplotlib.dates as mdates
+import calmap
+from matplotlib import pyplot as plt
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
+
+
+def freq_analysis_time_intervals(journey_df):
+    fig, axs = plt.subplots(3, 2, figsize=(18, 12))
+
+    # Hourly bike rentals
+    sns.countplot(x='hour', data=journey_df, color='#66c2a5', ax=axs[0, 0])
+    axs[0, 0].set_title('Total Bike Rentals by Hour of Day')
+    axs[0, 0].set_xticklabels([f'{x:02d}' for x in range(24)])
+    axs[0, 0].yaxis.get_major_formatter().set_scientific(False)
+
+    # Part of day bike rentals
+    sns.countplot(x='part_of_day', data=journey_df, color='#fc8d62', ax=axs[0, 1])
+    axs[0, 1].set_title('Total Bike Rentals by Part of Day')
+    axs[0, 1].set_xticklabels(['Early morning', 'Morning', 'Afternoon', 'Evening', 'Night'])
+    axs[0, 1].yaxis.get_major_formatter().set_scientific(False)
+
+    # Daily bike rentals
+    sns.countplot(x='day_of_week', data=journey_df, color='#8da0cb', ax=axs[1, 0])
+    axs[1, 0].set_title('Total Bike Rentals by Day of Week')
+    axs[1, 0].set_xticklabels(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+    axs[1, 0].yaxis.get_major_formatter().set_scientific(False)
+
+    # Monthly bike rentals
+    sns.countplot(x='month', data=journey_df, color='#e78ac3', ax=axs[1, 1])
+    axs[1, 1].set_title('Total Bike Rentals by Month')
+    axs[1, 1].set_xticklabels(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+    axs[1, 1].yaxis.get_major_formatter().set_scientific(False)
+
+    # Seasonal bike rentals
+    sns.countplot(x='season', data=journey_df, color='#a6d854', ax=axs[2, 0])
+    axs[2, 0].set_title('Total Bike Rentals by Season')
+    axs[2, 0].set_xticklabels(['Spring', 'Summer', 'Fall', 'Winter'])
+    axs[2, 0].yaxis.get_major_formatter().set_scientific(False)
+
+    # Yearly bike rentals
+    sns.countplot(x='year', data=journey_df, color='#ffd92f', ax=axs[2, 1])
+    axs[2, 1].set_title('Total Bike Rentals by Year')
+    axs[2, 1].yaxis.get_major_formatter().set_scientific(False)
+
+
+    plt.tight_layout()
+    return plt
+
+
+def time_series_decomposition(journey_df, decomposed):
+
+    # Set the size of the figure
+    fig, axs = plt.subplots(5, 1, figsize=(14, 12))
+
+    # Create a 'date' column that contains only the date part of the 'start_time'
+    journey_df['date'] = journey_df['start_date'].dt.date
+
+    # Group by date and count the number of journeys
+    journeys_per_date = journey_df.groupby(['date']).size().reset_index(name='count')
+
+    # 1. Plot the original data
+    axs[0].plot(decomposed.observed, label='Original')
+    axs[0].legend(loc='upper left')
+    axs[0].set_title("Observed")
+
+    # 2. Plot the original data with lockdown periods
+    axs[1].plot(journeys_per_date['date'], journeys_per_date['count'], label='Original with lockdown periods')
+    axs[1].xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    axs[1].xaxis.set_major_locator(mdates.YearLocator())
+    axs[1].axvspan('2020-03-23', '2020-07-03', color='orange', alpha=0.3, label='First National Lockdown')
+    axs[1].axvspan('2020-11-05', '2020-11-26', color='red', alpha=0.3, label='Second National Lockdown')
+    axs[1].axvspan('2021-01-06', '2021-03-08', color='purple', alpha=0.3, label='Third National Lockdown')
+    axs[1].legend()
+    axs[1].set_title('Observed with lockdown periods')
+
+
+    # 3. Plot the trend
+    axs[2].plot(decomposed.trend, label='Trend')
+    axs[2].legend(loc='upper left')
+    axs[2].set_title("Trend")
+
+    # 4. Plot the seasonality
+    axs[3].plot(decomposed.seasonal, label='Seasonality')
+    axs[3].legend(loc='upper left')
+    axs[3].set_title("Seasonality")
+
+    # 5. Plot the residuals
+    axs[4].plot(decomposed.resid, label='Residuals')
+    axs[4].legend(loc='upper left')
+    axs[4].set_title("Residuals")
+
+    plt.tight_layout()
+    return plt
+
+
+def correlation_plot(df, aggregation, title):
+    # aggregate the data based on desired frequency (e.g., daily)
+    aggregated_data = df.resample(aggregation, on='start_date').mean()
+
+    # calculate the correlation matrix
+    correlation_matrix = aggregated_data.corr()
+
+    # create the correlation plot
+    sns.set(style="white")
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
+    plt.title(title)
+
+    return plt
--- src/visualization/station_locations_vis.py
+++ src/visualization/station_locations_vis.py
@@ -4,37 +4,6 @@ import geopandas as gpd
 import branca.colormap as cm
 
 
-def create_station_map(bike_locs, london_coords):
-    """
-    This function creates an interactive map showing the locations of bike stations in London. 
-    It adds markers for each station and creates a heatmap layer based on the stations' geographical coordinates.
-
-    Args:
-        bike_locs (pd.DataFrame): A pandas dataframe containing the bike station locations. 
-                                  It must have 'id', 'name', 'borough', 'lat', and 'lon' columns.
-        london_coords (tuple): A tuple containing the latitude and longitude of London for centering the map.
-
-    Returns:
-        map (folium.folium.Map): A folium map object with added markers for each bike station and a heatmap layer.
-    """
-
-    map = folium.Map(location=london_coords, zoom_start=12, tiles='Stamen Toner')
-
-    # add markers for each bike station location
-    for index, row in bike_locs.iterrows():
-        popup_text = f"{row['name']} (id: {row['id']}) (borough: {row['borough']})"
-        marker = folium.CircleMarker(location=(row['lat'], row['lon']), popup=popup_text)
-        marker.add_to(map)
-
-    # add a heatmap layer
-    heat_data = [[row['lat'], row['lon']] for index, row in bike_locs.iterrows()]
-    heatmap = folium.FeatureGroup(heat_data)
-    heatmap.add_to(map)
-
-    return map
-
-
-
 def bar_chart_stations_per_borough(color_dict, borough_counts):
     """
     This function creates a bar chart representing the number of bike stations per borough.
@@ -60,20 +29,20 @@ def bar_chart_stations_per_borough(color_dict, borough_counts):
 
 
 
-def create_borough_map(london_coords, boroughs_geojson_path, color_dict):
+def create_borough_station_map(bike_locs, markerColor, color_dict, boroughs_geojson_path):
     """
-    Create a map of London boroughs with colored boundaries based on the provided density color dictionary.
+    Create a map with markers representing bike stations colored based on the borough.
 
     Args:
-        color_dict_density (dict): Dictionary mapping borough names to color values.
-        london_coords (tuple): Tuple of latitude and longitude specifying the center coordinates of London.
-        boroughs_geojson_path (str): Path to the GeoJSON file containing London borough boundaries.
-        save_path (str): Path to save the resulting HTML map file.
+        bike_locs (pandas.DataFrame): DataFrame containing bike station data.
+        markerColor (str): Color of the markers.
 
     Returns:
-        None
+        amap (folium.Map): Map with markers representing bike stations colored based on the borough.
+
     """
-    
+
+    # create borough map
     default_color = '#999999'
 
     def style_function(feature):
@@ -89,34 +58,19 @@ def create_borough_map(london_coords, boroughs_geojson_path, color_dict):
             'fillOpacity': 0.6,
         }
 
-    map = folium.Map(location=london_coords, zoom_start=11, tiles='Stamen Toner')
+    map = folium.Map(location=(51.5074, -0.1278), zoom_start=11, tiles='Stamen Toner')
 
     boroughs_geojson = gpd.read_file(boroughs_geojson_path)
     folium.GeoJson(boroughs_geojson, name='geojson', style_function=style_function).add_to(map)
 
-    return map
 
-
-def create_borough_station_map(bike_locs, base_map, markerColor):
-    """
-    Create a map with markers representing bike stations colored based on the borough.
-
-    Args:
-        bike_locs (pandas.DataFrame): DataFrame containing bike station data.
-        base_map (folium.Map): Base map to add markers to.
-        markerColor (str): Color of the markers.
-
-    Returns:
-        base_map (folium.Map): Map with markers representing bike stations colored based on the borough.
-
-    """
-
+    # add locations
     for index, row in bike_locs.iterrows():
         popup_text = f"{row['name']} (borough: {row['borough']}))"
         marker = folium.CircleMarker(location=(row['lat'], row['lon']), radius=1, popup=popup_text, color=markerColor, fill_opacity=0.7)
-        marker.add_to(base_map)
+        marker.add_to(map)
     
-    return base_map
+    return map
     
 
 
